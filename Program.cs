833c7033b5bbe18a1cb8201cdc6c7b8a697a7155
=DhairyaC <=dhairyachheda5@gmail.com>
2021-07-28 10:34:26 +0530

    Changes in program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace prjGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Good morining!");
         }
     }
 }
